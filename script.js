3ea33d64b805749041c902a1a0dc357d746171f9
jmscacc <123053543+jmscacc@users.noreply.github.com>
2023-01-19 17:05:54 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+
+const monthsarry = "";
+
+const month = ["January","February","March","April","May","June","July",
+                "August","September","October","November","December"];
+
+const first = "July 10 2017";
+
+function countdown(){
+    const firstDate = new Date(first);
+    const currentDate = new Date();
+    const currentMonthsarry = (currentDate.getTime() - firstDate.getTime()) / 2629746000;
+    document.getElementById("months").innerHTML = parseInt(currentMonthsarry) + 1;
+    document.getElementById("ordinal").innerHTML = nth(parseInt(currentMonthsarry));
+    const monthsarryMonth = currentDate.getMonth() + 1;
+    const monthsarryDate = new Date(month[monthsarryMonth] + " 10 " + currentDate.getFullYear());
+    const totalSeconds = (monthsarryDate.getTime() - currentDate.getTime()) / 1000;
+    const days = totalSeconds / 86400;
+    const hours = (days - parseInt(days))* 86400 / 3600;
+    const minutes = (hours - parseInt(hours))* 3600 / 60;
+    const seconds = (minutes - parseInt(minutes))* 60;
+
+    document.getElementById("days").innerHTML = parseInt(days);
+    document.getElementById("hours").innerHTML = parseInt(hours);
+    document.getElementById("minutes").innerHTML = parseInt(minutes);
+    document.getElementById("seconds").innerHTML = formatSeconds(parseInt(seconds));
+
+}
+
+function formatSeconds(seconds){
+    if(seconds < 10){
+        return "0"+ seconds;
+    }
+    return seconds
+}
+
+function nth(d) {
+    if (d > 3 && d < 21) return 'th';
+    switch (d % 10) {
+      case 1:  return "st";
+      case 2:  return "nd";
+      case 3:  return "rd";
+      default: return "th";
+    }
+}
+
+countdown();
+
+setInterval(countdown, 1000);
+
+
